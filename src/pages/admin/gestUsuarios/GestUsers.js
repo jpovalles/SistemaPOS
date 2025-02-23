8c016f3acad41abac97a5a7d9693cca166b59bc9
jpovalles <jpovalles@javerianacali.edu.co>
2025-02-22 22:27:14 -0500

    new icons for add user and delete user in admin view, new styles for users table in admin view

--- src/pages/admin/gestUsuarios/GestUsers.js
+++ src/pages/admin/gestUsuarios/GestUsers.js
@@ -5,9 +5,9 @@ import TopBarAdmin from '../../../components/TopBarAdmin';
 
 function GestUsers(){
     const data = [
-        { id: 1234, nombre: "Juan Pablo Ovalles Ceron", usuario: "jpoc", clave: "123456789", rol: "admin" },
-        { id: 1234, nombre: "Juan Ovalles", usuario: "jpoc", clave: "123456789", rol: "admin" },
-        { id: 1234, nombre: "Juan Ovalles", usuario: "jpoc", clave: "123456789", rol: "admin" },];
+        { nombre: "Juan Pablo Ovalles Ceron", usuario: "jpoc", clave: "123456789", rol: "admin" },
+        { nombre: "Juan Ovalles", usuario: "jpoc", clave: "123456789", rol: "admin" },
+        { nombre: "Juan Ovalles", usuario: "jpoc", clave: "123456789", rol: "admin" },];
     return(
         <div className="gest-users">
             <TopBarAdmin paginaActualAdmin="usuariosAdmin"/>
@@ -25,25 +25,28 @@ function GestUsers(){
             <table>
                 <thead>
                 <tr className='headerRow'>
-                    <th className="px-4 py-2 text-left">ID</th>
-                    <th className="px-4 py-2 text-left">Nombre</th>
-                    <th className="px-4 py-2 text-left">Usuario</th>
-                    <th className="px-4 py-2 text-left">Clave</th>
-                    <th className="px-4 py-2 text-left">Rol</th>
-                    <th className="px-4 py-2 text-left">Acciones</th>
+                    <th className="headerGest">Nombre</th>
+                    <th className="headerGest">Usuario</th>
+                    <th className="headerGest">Clave</th>
+                    <th className="headerGest">Rol</th>
+                    <th className="headerGest">Acciones</th>
                 </tr>
                 </thead>
                 <tbody>
                 {data.map((row, index) => (
                     <tr key={index}>
-                    <td className="px-4 py-2">{row.id}</td>
                     <td className="px-4 py-2">{row.nombre}</td>
                     <td className="px-4 py-2">{row.usuario}</td>
                     <td className="px-4 py-2">{row.clave}</td>
                     <td className="px-4 py-2">{row.rol}</td>
                     <td className="iconsRow">
-                        <p>Editar</p>
-                        <p>Eliminar</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="editIcon">
+                            <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="deleteIcon">
+                            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm3 10.5a.75.75 0 0 0 0-1.5H9a.75.75 0 0 0 0 1.5h6Z" clip-rule="evenodd" />
+                        </svg>
+
                     </td>
                     </tr>
                 ))}
