265e4f53ae5061d082f6ea454ea84bef82908e71
JuanOspina16 <juan12p16@gmail.com>
2025-03-08 15:05:50 -0500

    Modificar usuarios en la bd

--- src/pages/admin/gestUsuarios/GestUsers.js
+++ src/pages/admin/gestUsuarios/GestUsers.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import './GestUsers.css';
 import TopBarAdmin from "../../../components/TopBarAdmin";
 import { useState } from 'react';
-import {obtenerUsuarios, agregarUsuario, eliminarUsuario } from "../../../api"
+import {obtenerUsuarios, agregarUsuario, eliminarUsuario, actualizarUsuario } from "../../../api"
 
 
 function GestUsers(){
@@ -11,8 +11,8 @@ function GestUsers(){
     const [data, setData] = useState([]);
 
     const [newItem, setNewItem] = useState({ name: '', description: '' });
-    const [editingId, setEditingKey] = useState(null);
-    const [editItem, setEditItem] = useState({ name: '', description: '' });
+    const [editing, setEditing] = useState(null);
+    const [editItem, setEditItem] = useState([]);
 
     useEffect(() => {
         async function cargarUsuarios(){
@@ -32,17 +32,26 @@ function GestUsers(){
         }
     }
 
-    const handleEdit = (key) => {
-        setEditItem({ ...data[key] });
-        setEditingKey(key);
+    const handleEdit = (user) => {
+        setEditing(user.usuario);
+        setEditItem(user);
     };
 
-    const handleSave = (key) => {
-        setData({
-            ...data,
-            [key]: { ...editItem }
-        });
-        setEditingKey(null);
+    const handleSave = async () => {
+        const respuesta = await actualizarUsuario(
+            editing,
+            editItem.usuario,
+            editItem.clave,
+            editItem.nombre,
+            editItem.rol
+        )
+        if (respuesta.success){
+            const rolNombre = Object.keys(roles).find(key => roles[key] === parseInt(editItem.rol));
+            setData(data.map((c) => (c.usuario === editing ? {...editItem, rol: rolNombre}: c)));
+            setEditing(null);
+        }else{
+            alert(respuesta.message || "Error al modificar usuario")
+        }
     };
 
     const handleAdd = async(e) => {
@@ -117,11 +126,10 @@ function GestUsers(){
                     <tbody>
                     {data.map((user) => (
                         <tr key={user.usuario}>
-                        {editingId === user.usuario ? (
+                        {editing === user.usuario ? (
                             <>
                                 <td>
                                     <input
-                                        placeholder="Usuario"
                                         value={editItem.usuario}
                                         onChange={(e) => setEditItem({ ...editItem, usuario: e.target.value })}
                                     />
@@ -148,15 +156,15 @@ function GestUsers(){
                                     className="border p-2 rounded"
                                     >
                                     <option value="">Seleccionar Rol</option>
-                                    {roles.map(rol => (
-                                        <option key={rol} value={rol}>
-                                        {rol.charAt(0).toUpperCase() + rol.slice(1)}
+                                    {Object.keys(roles).map((clave) => (
+                                        <option key={clave} value={roles[clave]}>
+                                        {clave.charAt(0).toUpperCase() + clave.slice(1)}
                                         </option>
                                     ))}
                                 </select>
                                 </td>
                                 <td>
-                                    <div onClick={() => handleSave(user.usuario)}>
+                                    <div onClick={() => handleSave()}>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="saveIcon">
                                             <path d="M11.47 1.72a.75.75 0 0 1 1.06 0l3 3a.75.75 0 0 1-1.06 1.06l-1.72-1.72V7.5h-1.5V4.06L9.53 5.78a.75.75 0 0 1-1.06-1.06l3-3ZM11.25 7.5V15a.75.75 0 0 0 1.5 0V7.5h3.75a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h3.75Z" />
                                         </svg>
@@ -170,7 +178,7 @@ function GestUsers(){
                             <td className="px-4 py-2">{user.nombre}</td>
                             <td className="px-4 py-2">{user.rol}</td>
                             <td className="iconsRow">
-                                <div className={user.usuario} onClick={() => handleEdit(user.usuario)}>
+                                <div className={user.usuario} onClick={() => handleEdit(user)}>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="editIcon">
                                         <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
                                     </svg>
